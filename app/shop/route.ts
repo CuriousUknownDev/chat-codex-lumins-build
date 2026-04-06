d7daa48472d15f116339c3c197312837175e0488
CuriousUknownDev <aguirremichael2745@gmail.com>
2026-04-06 12:57:56 -0700

    Enhance footer with support links
    Added support links for tracking orders, returns, warranty claims, bulk pricing, and rebate help.

--- app/shop/route.ts
+++ app/shop/route.ts
@@ -719,20 +719,38 @@ a[href="/"].back-to-site:hover, header a[href="/"]:hover { color: #FFFFFF !impor
         <li><a href="#">Accessories</a></li>
       </ul>
     </div>
-    <div class="footer-col">
-      <h5>Support</h5>
-      <ul>
-        <li><a href="#">Track Order</a></li>
-        <li><a href="#">Returns</a></li>
-        <li>
-  <a href="mailto:info@luminslighting.com?subject=Lumins Lighting Warranty Claim&body=Company Name:%0AContact Name:%0APhone:%0AProduct SKU:%0AInvoice or PO Number:%0AIssue Description:%0A">
-    Warranty Claims
-  </a>
-</li>
-        <a href="mailto:quotes@luminslighting.com?subject=Bulk Pricing Request&body=Company Name:%0AProject Details:%0AQuantity Needed:%0A">Bulk Pricing</a>
-        <li><a href="#">Rebate Help</a></li>
-      </ul>
-    </div>
+   <div class="footer-col">
+  <h5>Support</h5>
+  <ul>
+    <li>
+      <a href="/track-order">Track Order</a>
+    </li>
+
+    <li>
+      <a href="mailto:info@luminslighting.com?subject=Lumins Lighting Return Request&body=Company Name:%0AContact Name:%0APhone:%0AOrder Number:%0AProduct SKU:%0AReason for Return:%0ACondition of Item:%0A">
+        Returns
+      </a>
+    </li>
+
+    <li>
+      <a href="mailto:info@luminslighting.com?subject=Lumins Lighting Warranty Claim&body=Company Name:%0AContact Name:%0APhone:%0AProduct SKU:%0AInvoice or PO Number:%0AIssue Description:%0A">
+        Warranty Claims
+      </a>
+    </li>
+
+    <li>
+      <a href="mailto:quotes@luminslighting.com?subject=Bulk Pricing Request&body=Company Name:%0AProject Details:%0AQuantity Needed:%0A">
+        Bulk Pricing
+      </a>
+    </li>
+
+    <li>
+      <a href="mailto:info@luminslighting.com?subject=Rebate Assistance Request&body=Company Name:%0ALocation:%0AUtility Provider:%0AProject Type:%0A">
+        Rebate Help
+      </a>
+    </li>
+  </ul>
+</div>
     <div class="footer-col">
       <h5>Company</h5>
       <ul>
